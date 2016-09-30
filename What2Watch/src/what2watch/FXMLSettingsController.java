baf79ab921f8f7acf2860716d60bdf25bb0f4f73
raph-u <raphael.bazzari@cpnv.ch>
2016-09-30 10:41:35 +0200

    Added functionnalities to the settings UI
    The settings UI can now be used to save the path leading to the folder containing his movies.
    
    Added a Preferences class that will store data such as the movie folder path.
    
    Added code that is now linked to the buttons in the UI
    
    Added an error label that is shown when the user tries to save a path prior to choosing a folder containing his movies

--- What2Watch/src/what2watch/FXMLSettingsController.java
+++ What2Watch/src/what2watch/FXMLSettingsController.java
@@ -5,6 +5,7 @@
  */
 package what2watch;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -13,6 +14,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 /**
@@ -32,28 +34,68 @@ public class FXMLSettingsController implements Initializable {
     private Button browseButton;
     @FXML
     private TextField pathTextField;
+    @FXML
+    private Label emptyPathErrorLabel;
+    
+    private UserPreferences prefs = new UserPreferences();
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        // For test purposes only 
+        // this.prefs.removePath();
+
+        // Filling the textfield with the saved movie folder path
+        if(! this.prefs.getPath().equals("")) {
+            this.pathTextField.setText(this.prefs.getPath());
+        }
     }    
 
     @FXML
     private void closeWindow(ActionEvent event) {
-        // Getting a reference to the settings window window and closing it
-        Stage stage = (Stage) cancelButton.getScene().getWindow();
-        stage.close();
+        closeStage(cancelButton);
     }
 
     @FXML
     private void setPath(ActionEvent event) {
+        // Making sure the user specified a path for his movie folder
+        if (!this.pathTextField.getText().equals("")) {
+            this.prefs.savePath(this.pathTextField.getText());
+            this.emptyPathErrorLabel.setVisible(false);
+            
+            // Closing the settings window
+            closeStage(pathButton);
+        } else {
+            this.emptyPathErrorLabel.setVisible(true);
+        }
     }
 
     @FXML
     private void browseFolders(ActionEvent event) {
+        final DirectoryChooser directoryChooser = new DirectoryChooser();
+        Stage chooserStage = new Stage();
+        
+        configureDirectoryChooser(directoryChooser);
+        
+        File file = directoryChooser.showDialog(chooserStage);
+        
+        // Filling the path textfield if the user chose a directory
+        if (file != null) {
+            this.pathTextField.setText(file.getAbsolutePath());
+        }
     }
     
+    private static void configureDirectoryChooser(final DirectoryChooser directoryChooser) {      
+        directoryChooser.setTitle("Select directory");
+        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+    }
+    
+    private static void closeStage(Button button) {
+        // Getting a reference to the settings window and closing it
+        Stage stage = (Stage) button.getScene().getWindow();
+        stage.close();
+    }
 }
