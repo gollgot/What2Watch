a934a8b820a5e388c6b3c5783adae465ba8464dc
raph-u <raphael.bazzari@cpnv.ch>
2016-11-26 15:46:12 +0100

    Fixed a bug in search by title process
    Fixed a bug preventing the movie listView from displaying the original movie list after when the search textfield is empty.

--- What2Watch/src/what2watch/SearchHandler.java
+++ What2Watch/src/what2watch/SearchHandler.java
@@ -16,31 +16,44 @@ import javafx.scene.control.ListView;
  */
 public class SearchHandler {
     private static ListView<Movie> movieListView;
-    private static ObservableList<Movie> originaleMovieList;
+    private static ObservableList<Movie> originalMovieList;
     
-    // Defines the list view that will be updated by this class methods
+    // Provides the class with informations that will be used in its methods
+    // in order to process a movie search
     public static void initializeSearchHandler (ListView<Movie> listView, ObservableList<Movie> movieList) {
        movieListView = listView; 
-       originaleMovieList = movieList;
+       originalMovieList = movieList;
     }
     
+    // Compares the title of every movies contained in the listView on the main Window
+    // against the search term provided in parameter.
+    // Either displays a list of movies that match the search or the original one
     public static void findMoviesByTitle(String searchTerm) {
-        ObservableList<Movie> matchingMovies = FXCollections.observableArrayList();
+        // Eventually holds the list of films that will be displayed in the listView
+        ObservableList<Movie> finalList = FXCollections.observableArrayList();
         
-        for(Movie currentMovie: originaleMovieList) {
-            String movieName = currentMovie.getTitle();
-            if (searchTerm.equals("")) {
-                System.out.println("1" + originaleMovieList);
-                movieListView.setItems(originaleMovieList);
-            } else {
+        // Making sure there is a term to base the search on
+        if (!searchTerm.equals("")) {
+            ObservableList<Movie> matchingMovies = FXCollections.observableArrayList();
+            
+            // Comparing the search term against each title of movies in the list
+            for (Movie currentMovie : originalMovieList) {
+                String movieName = currentMovie.getTitle();
                 if (movieName.toLowerCase().contains(searchTerm.toLowerCase())) {
                    matchingMovies.add(currentMovie); 
                 }
             }
+            // Setting the final with a list of movies that match the search term
+            finalList = matchingMovies;
+            
+        } else {
+            // No search term to compare against -> setting the final list to the original movie list
+            finalList = originalMovieList;
         }
         
-        // Updating the movie list movies matching the search terms
-        movieListView.setItems(matchingMovies);
+        // Updating the list view with either a list of movies matching the search term
+        // or the list that was originally displayed before the search occured
+        movieListView.setItems(finalList);
     }
     
 }
